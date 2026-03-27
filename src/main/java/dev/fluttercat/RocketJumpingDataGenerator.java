c084af0f8981663f5913bf2524766a124ea40a39
FlutterCat <156819167+FlutterCat555@users.noreply.github.com>
2026-03-27 19:01:11 -0400

    Initial commit

---
+++ src/main/java/dev/fluttercat/RocketJumpingDataGenerator.java
@@ -0,0 +1,11 @@
+package dev.fluttercat;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RocketJumpingDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/dev/fluttercat/mixin/FireworkRocketEntityMixin.java
@@ -0,0 +1,52 @@
+package dev.fluttercat.mixin;
+
+import dev.fluttercat.RocketJumping;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.projectile.FireworkRocketEntity;
+import net.minecraft.world.entity.projectile.Projectile;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+
+@Mixin(FireworkRocketEntity.class)
+public abstract class FireworkRocketEntityMixin extends Projectile {
+    public FireworkRocketEntityMixin(EntityType<? extends @NotNull Projectile> type, Level level) {
+        super(type, level);
+    }
+
+    @Redirect(
+            method = "dealExplosionDamage",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/entity/LivingEntity;hurtServer(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/damagesource/DamageSource;F)Z",
+                    ordinal = 1
+            )
+    )
+    private boolean dealExplosionDamage(LivingEntity target, ServerLevel level, DamageSource source, float damage) {
+        Vec3 rocketPos = this.position();
+        Vec3 targetPos = target.position();
+        Vec3 direction = targetPos.subtract(rocketPos).normalize();
+        double scale;
+        if(target==this.getOwner()) {
+            scale = 1.75;
+            damage = damage / 2;
+            direction = new Vec3(direction.x * 2, direction.y, direction.z * 2 );
+            target.setOnGround(false);
+            target.setDeltaMovement(direction.scale(scale));
+            target.hurtMarked = true;
+        }
+
+
+
+
+
+        return target.hurtServer(level, source, damage);
+    }
+}
